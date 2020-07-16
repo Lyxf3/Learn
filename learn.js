eaf6edad85eb7c2ff949b612895e9cad8f275fa9
Windows <dmitry.zhigiliy@gmail.com>
2020-07-16 11:19:38 +0300

    First

---
+++ learn.js
@@ -0,0 +1,16 @@
+"use strict";
+
+let incr = 10,
+    decr = 10;
+
+let name = {
+    dsf: 123123,
+    sada:312
+}
+
+console.log(name.dsf);
+++incr;
+--decr;
+
+console.log(incr);
+console.log(decr);
